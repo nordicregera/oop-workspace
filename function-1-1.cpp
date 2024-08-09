5e49d4cb03bdbe0f474b28d83452cd49f06a78d9
nordicregera <a1933335@adelaide.edu.au>
2024-08-09 05:33:00 +0000

    111

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,7 +2,7 @@
 double arrayMin(double* array, int size) {
     double min=array[0];
     for (int i=1;i<size;i++) {
-        if (array[i]<min) {
+        if (min>array[i]) {
             array[i]=min;
         }
 
