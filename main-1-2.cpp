835fa8ce228e961228ca9de77b4ae41dca4116a6
nordicregera <a1933335@adelaide.edu.au>
2024-09-04 08:46:13 +0000

    1

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,9 +1,11 @@
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
 #include "ParkingLot.h"
-#include "Vehicle.h"
 #include <iostream>
 
 int main() {
-    ParkingLot lot(10); 
+    ParkingLot lot(10);  // 停车场容量为 10
 
     while (true) {
         int choice;
@@ -19,18 +21,18 @@ int main() {
 
             Vehicle* vehicle = nullptr;
             if (type == 1) {
-                vehicle = new Car(id);
+                vehicle = new Car(id);  // 确保类型正确
             } else if (type == 2) {
-                vehicle = new Bus(id);
+                vehicle = new Bus(id);  // 确保类型正确
             } else if (type == 3) {
-                vehicle = new Motorbike(id);
+                vehicle = new Motorbike(id);  // 确保类型正确
             } else {
                 std::cout << "Invalid vehicle type!" << std::endl;
                 continue;
             }
 
             if (!lot.parkVehicle(vehicle)) {
-                delete vehicle;  
+                delete vehicle;  // 如果无法停车，需要删除对象
             }
 
         } else if (choice == 2) {
