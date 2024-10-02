338f732764f44c403c85edfacff21918090d8fc6
nordicregera <a1933335@adelaide.edu.au>
2024-10-02 00:07:15 +0000

    1

--- Invoice.h
+++ Invoice.h
@@ -5,29 +5,21 @@
 
 class Invoice {
 private:
-    std::string invoiceId;
-    double dollarsOwed;
+    std::string invoiceId;  
+    double dollarsOwed;     
 
 public:
+    
+    Invoice(const std::string& id);
 
-    Invoice(const std::string& id) : invoiceId(id), dollarsOwed(0.0) {}
+   
+    void addServiceCost(double costDollars);
 
+    
+    double getDollarsOwed() const;
 
-    void addServiceCost(double costDollars) {
-        if (costDollars > 0) {
-            dollarsOwed += costDollars;
-        }
-    }
-
-
-    double getDollarsOwed() const {
-        return dollarsOwed;
-    }
-
-
-    std::string getInvoiceId() const {
-        return invoiceId;
-    }
+   
+    std::string getInvoiceId() const;
 };
 
 #endif
